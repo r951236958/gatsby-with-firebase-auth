385a9c7b37eb13d03734c6469fb26a7016a5bde3
nick <r951236958@gmail.com>
2021-03-02 16:54:04 +0800

    更新thankspage

--- src/pages/thankyou.jsx
+++ src/pages/thankyou.jsx
@@ -1,18 +1,18 @@
-import Button from "gatsby-theme-material-ui"
-import React from 'react'
-import Layout from '../components/Layout'
+import { Button } from "gatsby-theme-material-ui"
+import React from "react"
+import Layout from "../components/Layout"
 import SEO from "../components/SEO"
 
-const ThanksPage = () => {
-  const title = 'Thank You!!'
+const ThankYou = () => {
+  const title = "Thank You!!"
   return (
     <Layout siteTitle={title}>
       <SEO title={title} />
-      <Button color="primary" variant="contained" to="/form/">
+      <Button color='primary' variant='contained' to='/form/'>
         返回表格
       </Button>
     </Layout>
   )
 }
 
-export default ThanksPage
+export default ThankYou
