29e16720790e94bdaa66f79d41a94b64bda6d412
nick <r951236958@gmail.com>
2021-03-02 14:01:34 +0800

    再試一次

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,3 +1,8 @@
+import "@reach/dialog/styles.css"
 import "firebase/auth"
 import "firebase/firestore"
 import "firebase/functions"
+import "firebaseui/dist/firebaseui.css"
+import "prismjs/themes/prism-okaidia.css"
+import "./src/styles/global.scss"
+import "./src/styles/index.css"
