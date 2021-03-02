ab40e02a48b0655bfdaf056f4c9dc9a612512d1a
nick <r951236958@gmail.com>
2021-03-02 12:38:10 +0800

    刪除不需要的區塊

--- src/pages/thankyou.jsx
+++ src/pages/thankyou.jsx
@@ -1,8 +1,7 @@
+import Button from "gatsby-theme-material-ui"
 import React from 'react'
 import Layout from '../components/Layout'
-import SEO from '../components/SEO'
-import TextField from '../components/TextField'
-import Button from 'gatsby-theme-material-ui'
+import SEO from "../components/SEO"
 
 const ThanksPage = () => {
   const title = 'Thank You!!'
@@ -12,31 +11,6 @@ const ThanksPage = () => {
       <Button color="primary" variant="contained" to="/form/">
         返回表格
       </Button>
-      <div className="block max-w-1/2 my-4">
-        <TextField
-          id="test-input"
-          label="hi-label-here"
-          name="im-input"
-          type="text"
-          placeholder="placeholder"
-          className="w-12"
-        />
-      </div>
-      <div className="md-input-main">
-        <div className="md-input-box">
-          <input
-            id="fullName"
-            name="fullName"
-            type="text"
-            className="md-input"
-            placeholder=" "
-          />
-          <label htmlFor="fullName" className="md-label">
-            Full Name
-          </label>
-          <div className="md-input-underline"></div>
-        </div>
-      </div>
     </Layout>
   )
 }
